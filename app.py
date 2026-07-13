93b1be2c4266c7f927877581975882603ab7431d
Prakriti Bhandari <prakritibhandari555@gmail.com>
2026-07-13 07:57:50 +0545

    Load model from pickle and clean up code

--- app.py
+++ app.py
@@ -4,30 +4,33 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 import streamlit as st
+import pickle
 
-credited_card_df=pd.read_csv(r'C:/Users/LENOVO/Desktop/Detecting Credit Card Fraud/creditcard.csv.zip')
+# credited_card_df=pd.read_csv(r'C:/Users/LENOVO/Desktop/Detecting Credit Card Fraud/creditcard.csv.zip')
 
-legit=credited_card_df[credited_card_df.Class==0]
-fraud=credited_card_df[credited_card_df.Class==1]
+# legit=credited_card_df[credited_card_df.Class==0]
+# fraud=credited_card_df[credited_card_df.Class==1]
 
-legit_sample=legit.sample(n=len(fraud),random_state=2)
-credited_card_df=pd.concat([legit_sample,fraud],axis=0)
+# legit_sample=legit.sample(n=len(fraud),random_state=2)
+# credited_card_df=pd.concat([legit_sample,fraud],axis=0)
 
-X=credited_card_df.drop('Class',axis=1)
-Y=credited_card_df['Class']
-X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.1,stratify=Y, random_state=2)
-model = LogisticRegression(
-    solver='liblinear',
-    max_iter=1000,
-    random_state=42
-)
+# X=credited_card_df.drop('Class',axis=1)
+# Y=credited_card_df['Class']
+# X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.1,stratify=Y, random_state=2)
+# model = LogisticRegression(
+#     solver='liblinear',
+#     max_iter=1000,
+#     random_state=42
+# )
 
 
-model.fit(X_train, Y_train)
+# model.fit(X_train, Y_train)
 
-#evaluate model performance
-train_acc=accuracy_score(model.predict(X_train),Y_train)
-test_acc=accuracy_score(model.predict(X_test),Y_test)
+# #evaluate model performance
+# train_acc=accuracy_score(model.predict(X_train),Y_train)
+# test_acc=accuracy_score(model.predict(X_test),Y_test)
+
+model = pickle.load(open("model.pkl", "rb"))
 
 #web app
 st.title("Credit Card Fraud Detection Model")
